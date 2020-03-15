4f9b6f3ba29f892a414c9933bb109b2bd940ca65
Drew Adorno <drew.adorno@gmail.com>
2020-03-15 10:57:18 -0400

    Created Models book and review

--- apps/app1/models.py
+++ apps/app1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title= models.CharField(max_length=255)
+    author=models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Review(models.Model):
+    review=models.CharField(max_length=255)
+    rating=models.IntegerField(max=5)
+    user=models.ForeignKey('log_reg.User', related_name='reviews')
+    book = models.ForeignKey(Book, related_name="reviews")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
